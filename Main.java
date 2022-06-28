d28d1bc84bc9324746702b0a6fa9e9a4525e8a77
kirill160 <81866092+kirill160@users.noreply.github.com>
2022-06-28 20:51:56 +0300

    fvf

---
+++ Main.java
@@ -0,0 +1,100 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Дана сторона квадрата a. Найти его периметр P = 4·a
+//        int a = 5;
+//        int p = 4 * a;
+//        System.out.println(p);
+        //Дана сторона квадрата a. Найти его площадь S = a*2
+
+//        int a = 4;
+//        int S =(int) Math.pow(a,2);
+//        System.out.println(S);
+        //Даны стороны прямоугольника a и b. Найти его площадь S = a·b и
+        //периметр P = 2·(a + b).
+//        int a = 7;
+//        int b = 3;
+//        int s = a * b;
+//        int p = 2 * (a + b);
+//        System.out.println("площадь прямоугольника равна : " + s + "/n периметр прямоугольника равен : " + p);
+        //Дан диаметр окружности d. Найти ее длину L = π·d. В качестве
+        //значения π использовать 3.14.
+//        int d = 10;
+//        double pi = Math.PI;
+//        int l = (int) ( d * pi);
+//        System.out.println(l);
+        //Дана длина ребра куба a. Найти объем куба V = a3 и площадь его поверхности S = 6·a2
+//            double a = 7;
+//            double v = Math.pow(a,3);
+//            double s = 6 * Math.pow(a,2);
+//        System.out.println(v);
+        //Даны длины ребер a, b, c прямоугольного параллелепипеда.
+        // Найтиего объем V = a·b·c и площадь поверхности S = 2·(a·b + b·c + a·c).
+//        int a = 25;
+//        int b = 36;
+//        int c = 15;
+//        int V = a * b * c;
+//        int S = 2 * (a * b + b * c + a * c);
+//        System.out.println("объем равен : " + V + "\nПлощадь равна : " + S);
+        //Найти длину окружности L и площадь круга S заданного радиуса R:
+        //L = 2·π·R, S = π·R2
+        //В качестве значения π использовать 3.14.
+//        double R = 10;
+//        double L = 2 * Math.PI * R;
+//        double S = Math.PI * Math.pow(R,2);
+        //Даны два числа a и b. Найти их среднее арифметическое: (a + b)/2
+//        int a = 5;
+//        int b = 7;
+//        int summ = (a + b) /2;
+        //Даны два неотрицательных числа a и b.
+        // Найти их среднее геометрическое, то есть квадратный корень из их произведения: √a·b.
+//        int a = 3;
+//        int b = 4;
+//        double c = Math.sqrt(a * b);
+//        System.out.println(c);
+        //Даны два ненулевых числа. Найти сумму, разность, произведение и
+        //частное их квадратов.
+//        double a = 7;
+//        double c = 3;
+//        if (a != 0 && c != 0){
+//            double summ =  (a + c);
+//            System.out.println(summ);
+//            double prod = a * c;
+//            System.out.println(prod);
+//            double diff = a - c;
+//            System.out.println(diff);
+//            double quot = a / c;
+//            System.out.println(quot);
+//            double prodPow = Math.pow(a, 2 ) * Math.pow(c,2);
+//            System.out.println(prodPow);
+//        }
+        // Даны два ненулевых числа. Найти сумму, разность, произведение и
+        //частное их модулей.
+        int a = 7;
+        int c = 4;
+        if (a != 0 && c != 0 ){
+                int summ = a + c;
+                System.out.println(summ);
+                int diff = a - c;
+                System.out.println(diff);
+                int prod = a * c;
+            System.out.println(prod);
+
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
