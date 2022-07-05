8bcafba9d6f58d4782085bcde14d5abc3b4b19ee
kirill160 <81866092+kirill160@users.noreply.github.com>
2022-07-05 21:39:15 +0300

    do at Home

--- MonsterGenerator.java
+++ MonsterGenerator.java
@@ -1,8 +1,11 @@
 package com.company.lesson1.ClassWork;
 
+import static com.company.lesson1.ClassWork.Monster.MonsterType.Dragon;
+
 public class MonsterGenerator {
     private static Monster generateMonster(){
-        return new Monster(Monster.MonsterType.values());
+        Monster dragon = new Monster("Dragon", 100, Monster.MonsterType.valueOf("Dragon"));
+        return dragon;
     }
 
     public static double getRandomNumber(int min, int max) {
