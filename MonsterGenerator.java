a3fffe15b08af608afb8e651f077e12203f0fba7
kirill160 <81866092+kirill160@users.noreply.github.com>
2022-07-07 21:35:19 +0300

    home

--- MonsterGenerator.java
+++ MonsterGenerator.java
@@ -1,15 +1,30 @@
 package com.company.lesson1.ClassWork;
 
-import static com.company.lesson1.ClassWork.Monster.MonsterType.Dragon;
+import com.company.lesson1.ClassWork.Monster.MonsterType;
+
+import java.util.Random;
 
 public class MonsterGenerator {
-    private static Monster generateMonster(){
-        Monster dragon = new Monster("Dragon", 100, Monster.MonsterType.valueOf("Dragon"));
-        return dragon;
+    public static Monster generateMonster(){
+       Monster randomMonster = new Monster("MAX_MONSTER_TYPES",getRandomNumber(0,100),MonsterType.MAX_MONSTER_TYPES);
+        Random random = new Random();
+        int numberOfMonster = random.nextInt(2);
+        if (numberOfMonster == 0){
+            randomMonster = new Monster("Dragon", getRandomNumber(0,100), MonsterType.Dragon);
+            return randomMonster;
+        }else if (numberOfMonster == 1){
+            randomMonster = new Monster("Orc", getRandomNumber(0,100), MonsterType.Orc);
+            return randomMonster;
+        }else if(numberOfMonster == 2){
+            randomMonster = new Monster("Slime",  getRandomNumber(0,100),MonsterType.Slime);
+            return randomMonster;
+        }
+
+        return randomMonster;
     }
 
-    public static double getRandomNumber(int min, int max) {
-        double randomNumber = min + Math.random() * max;
+    public static int getRandomNumber(int min, int max) {
+        int randomNumber =(int) (min + Math.random() * max);
         return randomNumber;
 
     }
